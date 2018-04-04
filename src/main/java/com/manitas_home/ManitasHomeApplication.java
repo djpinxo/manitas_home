e94620c43785413a5a5f58250cf28ae9ca3e88df
david <ict2david@gmail.com>
2018-04-05 00:11:47 +0200

    arreglo anterior

---
+++ src/main/java/com/manitas_home/ManitasHomeApplication.java
@@ -0,0 +1,12 @@
+package com.manitas_home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManitasHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManitasHomeApplication.class, args);
+	}
+}
