6bb24d0990b128f1a7c3a0e8f1fd06283c606376
david <ict2david@gmail.com>
2018-06-10 19:51:39 +0200

    limpieza codigo

--- src/main/java/com/manitas_home/controller/HomeController.java
+++ src/main/java/com/manitas_home/controller/HomeController.java
@@ -3,19 +3,17 @@ package com.manitas_home.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.manitas_home.repositories.AdministradorRepository;
-import com.manitas_home.repositories.MensajeRepository;
 import com.manitas_home.domain.Usuario;
 import com.manitas_home.funciones.funcionstart;
+import com.manitas_home.repositories.AdministradorRepository;
+import com.manitas_home.repositories.MensajeRepository;
 
 @Controller
-public class HomeController /*implements ErrorController*/{
+public class HomeController{
 	@Autowired
 	private AdministradorRepository RAdministrador;
 	@Autowired
@@ -37,12 +35,10 @@ public class HomeController /*implements ErrorController*/{
 	////////////////////////////////
 	@GetMapping("/")
 	public String index(HttpSession session,ModelMap m) {
-		
-			m.put("view","home/index");//carga la pagina por defecto
+			m.put("view","home/index");
 			m.put("usuarioactivo", session.getAttribute("user"));
 			if(session.getAttribute("user")!=null)
-			m.put("usuarioemails",RMensaje.countByDestinatarioAndLeido(((Usuario)session.getAttribute("user")).getEmail(),false));
-		//return funciones.funcionstart.funcionArranque(session,m,this.RAdministrador,"views/_t/main");
+				m.put("usuarioemails",RMensaje.countByDestinatarioAndLeido(((Usuario)session.getAttribute("user")).getEmail(),false));
 			return hayadmin("views/_t/main", "redirect:/administrador/crear");
 	}
 	private String hayadmin(String existeDestino,String noExisteDestino){
